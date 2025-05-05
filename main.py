d3fe7846a9a1139a95103a2159cd0ae492859df2
John Andre Yap <johnandresyap510@gmail.com>
2025-05-05 20:15:00 +0800

    Added main and util files

---
+++ main.py
@@ -0,0 +1,11 @@
+def display_menu():
+    print("Menu goes here.")
+
+def main():
+    print("--- WELCOME TO HAMILTONIAN CYCLES HEURISTIC ---")
+    display_menu()
+
+# install python
+
+if __name__ == "__main__":
+    main()
